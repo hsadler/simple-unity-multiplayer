75ef66987d3737aeebd36bc7e2675a6967834b57
Harry Sadler <harrysadlermusic@gmail.com>
2022-01-04 23:40:53 +0100

    add basic local runnable go websocket server

---
+++ GameServer/go.mod
@@ -0,0 +1,7 @@
+module server
+
+go 1.17
+
+require github.com/gorilla/websocket v1.4.2
+
+require github.com/google/uuid v1.3.0
---
+++ GameServer/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags)
+	// handle client connections
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// upgrade request to websocket and use default options
+		upgrader := websocket.Upgrader{}
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("Request upgrade error:", err)
+			return
+		}
+		ws.WriteMessage(1, []byte("hi"))
+	})
+	// run the server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	addr := flag.String("addr", "0.0.0.0:"+port, "http service address")
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("Server start error:", err)
+	}
+}
