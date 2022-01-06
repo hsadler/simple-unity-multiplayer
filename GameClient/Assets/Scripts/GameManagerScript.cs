011fdee4b3057a2032fa319f2220e82a3d38f69b
Harry Sadler <harrysadlermusic@gmail.com>
2022-01-06 11:43:17 +0100

    add code todo comments to game manager script in client

--- GameClient/Assets/Scripts/GameManagerScript.cs
+++ GameClient/Assets/Scripts/GameManagerScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    // TODO:
+    //- procedurally build environment
+    //- create serializable game-state object
+    //- create game-state-synch service:
+    //      - synchs game state object to server
+    //      - synchs game state object from server
+    //- receive player inputs:
+    //      - cause scene changes
+    //      - mutate game-state
+    //- event hooks:
+    //      - received update of game-state from server must update client
+    //          game-state and scene
+    //      - mutations to game-state from scene must automatically call for a
+    //          synch to server
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+
 }
